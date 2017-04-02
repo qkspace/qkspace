37528681687da3d183fe308042e2e1ac0f41a625
Vadim Venediktov <install.vv@gmail.com>
2017-04-02 20:24:56 +0300

    Project show accesible without login

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
