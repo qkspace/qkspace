dd695095aa0f2d74de578a14913cf71fbfd2dc30
Eugene Zolotarev <eugzol@gmail.com>
2017-04-26 18:00:39 +0300

    I18n integration & file layout

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module Dostoevsky
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.available_locales = %w(en ru)
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
