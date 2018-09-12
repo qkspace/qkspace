63a305270f06308b3912b33f95c76ec9f2b42364
vergil <vergil.sm@yandex.ru>
2018-09-12 15:19:46 +0300

    Markdown hint locale application_helper fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,15 +20,15 @@ module ApplicationHelper
   end
 
   def markdown_hint_link
-    href = link_to t('blocks.markdown_hint_a'), '#', data: {
+    href = link_to t('markdown_hint_a'), '#', data: {
       component: 'modal',
       target: '#markdown-modal-window'
     }
-    t('blocks.markdown_hint_link', href: href).html_safe
+    t('markdown_hint_link', href: href).html_safe
   end
 
   def markdown_hint
-    t('blocks.markdown_hint_link', href: t('blocks.markdown_hint_a'))
+    t('markdown_hint_link', href: t('markdown_hint_a'))
   end
 
   def markdown_partial
