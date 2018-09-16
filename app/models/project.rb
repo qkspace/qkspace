7f2fd9265f65885052f0923ee71c96d1a7fa9ee3
Aleksandr Klimenkov <prisioner@mail.ru>
2018-09-16 11:54:12 +0300

    Fix: A project without first page bug fixed (#27)

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,13 +2,15 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :pages, -> { ordered }, dependent: :delete_all
 
+  before_validation :generate_first_page, on: :create
+
+  validates_associated :pages, on: :create
+
   validates :title, :slug, presence: true
   validates :slug, format: { with: /\A[a-z0-9-]+\z/ }, uniqueness: true
 
   validates :google_analytics_tracker_id,
     format: { with: /\AUA-\d*-\d*\z/, allow_blank: true }
-
-  before_create :generate_first_page
 
   def generate_first_page
     pages.build(title: title, source: '')
