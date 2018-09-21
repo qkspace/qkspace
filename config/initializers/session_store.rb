c7d682cbaf0c25673c9acd604ea61c7e580e7f49
Vadim Venediktov <install.vv@gmail.com>
2018-09-21 23:21:27 +0300

    Session store name changed

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store,
-                                       key: '_dostoevsky_session',
+                                       key: '_qkspace_session',
                                        domain: :all,
                                        tld_length: 2
