bcf515278b667220de3225dbb0743868e1bf78b6
Eugene Zolotarev <eugzol@gmail.com>
2018-09-29 22:12:43 +0300

    Change locale link with flag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include HttpAcceptLanguage::AutoLocale
+  before_action :set_locale
 
   protect_from_forgery with: :reset_session
 
@@ -28,5 +28,19 @@ class ApplicationController < ActionController::Base
     uri = public_project_page_uri(project)
     uri.path = "/" << page.slug
     uri.to_s
+  end
+
+  def set_locale
+    new_locale =
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        params[:locale]
+      elsif session[:locale].present?
+        session[:locale]
+      else
+        http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
+      end
+
+    session[:locale] = new_locale
+    I18n.locale = new_locale
   end
 end
