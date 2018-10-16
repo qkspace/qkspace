2c27c469e2fb1227679232d0aa5329755086073b
Aleksandr Klimenkov <aleksandr.klimenkov@binomo.com>
2018-10-17 00:54:28 +0300

    restrict project slugs

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,12 +2,17 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :pages, -> { ordered }, dependent: :delete_all
 
+  SLUG_BLACK_LIST = %w[about admin api billing blog buy demo dev download email faq ftp help
+                       imap mail me my pop pop3 pricing sftp shop smtp staging support wiki
+                       ww www wwww].freeze
+
   before_validation :generate_first_page, on: :create
 
   validates_associated :pages, on: :create
 
   validates :title, :slug, presence: true
-  validates :slug, format: { with: /\A[a-z0-9-]+\z/ }, uniqueness: true
+  validates :slug, format: { with: /\A[a-z0-9-]+\z/ }, uniqueness: true,
+                   length: { minimum: 5 }, exclusion: { in: SLUG_BLACK_LIST }
 
   validates :google_analytics_tracker_id,
     format: { with: /\AUA-\d*-\d*\z/, allow_blank: true }
