7e5d6d43f9ae5783003aac2a0e28d683da62e9ab
Aleksandr Klimenkov <prisioner@mail.ru>
2018-10-17 01:35:29 +0300

    Add project slug hint (#50)

---
+++ app/services/slug_checker.rb
@@ -0,0 +1,17 @@
+class SlugChecker
+  def self.call(slug:, domain:)
+    project = Project.new(slug: slug)
+    project.validate
+
+    slug_available = project.errors[:slug].empty?
+    translation_key = "helpers.projects.slug_#{slug_available ? 'available' : 'not_available'}"
+    message = I18n.t(translation_key)
+
+    {
+      slug: slug,
+      domain: domain,
+      message: message,
+      availability: slug_available,
+    }
+  end
+end
