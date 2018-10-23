75ac05a3c4dc9e25601906023ecf96b8f631df76
Aleksandr Klimenkov <aleksandr.klimenkov@binomo.com>
2018-10-24 00:03:48 +0300

    add letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -52,5 +55,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'qkspace.localhost', port: 3000 }
 end
