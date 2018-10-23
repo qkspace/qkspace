eca06bc6f1dd7bd207ca0e0ad955633d35b8c088
Aleksandr Klimenkov <aleksandr.klimenkov@binomo.com>
2018-10-24 00:13:40 +0300

    config production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "dostoevsky_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'qkspace.com' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
