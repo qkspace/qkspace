25600efa1951ebafa5a4541141ac67385d91aacc
Eugene Zolotarev <eugzol@gmail.com>
2018-10-29 22:39:10 +0300

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git pull &&
+bundle install --deployment --without development test &&
+bundle exec rake assets:precompile &&
+RAILS_ENV=production bundle exec rake db:migrate &&
+RAILS_ENV=production bundle exec rake data:migrate &&
+touch tmp/restart.txt
