92be443e7877226f4ceed6ad9331db158edcc5f2
Eugene Zolotarev <eugzol@gmail.com>
2018-12-23 17:52:25 +0300

    Hotfix: Removed bugged migration

--- db/migrate/20181222221504_remove_excess_indexes.rb
+++
@@ -1,6 +0,0 @@
-class RemoveExcessIndexes < ActiveRecord::Migration[5.2]
-  def change
-    remove_index :project_collaborations, name: "index_project_collaborations_on_project_id"
-    remove_index :sessions, name: "authenticatable"
-  end
-end
