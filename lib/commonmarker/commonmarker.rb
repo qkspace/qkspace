86a718000308ba1dadbea2b2f266e815efe1b15a
gargantua <istroganovi@gmail.com>
2019-01-23 19:26:31 +0300

    Add anchors for headers

---
+++ lib/commonmarker/commonmarker.rb
@@ -0,0 +1,10 @@
+require 'commonmarker'
+
+class MyHtmlRenderer < CommonMarker::HtmlRenderer
+  def header(node)
+    block do
+    out("<h", node.header_level, " id=\"", I18n.transliterate(node.first_child.string_content).parameterize.presence,
+        "\">", :children, "</h", node.header_level, ">")
+    end
+  end
+end
