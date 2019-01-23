a6e4d7a5bdf2aa915c98d74ab6168fd129fc6693
Igor Stroganov <40701117+Gargantua88@users.noreply.github.com>
2019-01-23 19:31:11 +0300

    Update commonmarker.rb

--- lib/commonmarker/commonmarker.rb
+++ lib/commonmarker/commonmarker.rb
@@ -3,8 +3,8 @@ require 'commonmarker'
 class MyHtmlRenderer < CommonMarker::HtmlRenderer
   def header(node)
     block do
-    out("<h", node.header_level, " id=\"", I18n.transliterate(node.first_child.string_content).parameterize.presence,
-        "\">", :children, "</h", node.header_level, ">")
+      out("<h", node.header_level, " id=\"", I18n.transliterate(node.first_child.string_content).parameterize.presence,
+          "\">", :children, "</h", node.header_level, ">")
     end
   end
 end
