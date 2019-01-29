fd2d5ac49b5822773e092adfcd552e04e38a427f
gargantua <istroganovi@gmail.com>
2019-01-29 14:07:01 +0300

    refactor page test

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Page, type: :model do
-  context 'markup' do
-    let(:page_with_russian_headers) { FactoryBot.create(:page, source: "# Привет, мир 2!\r\nHello, world!") }
-    let(:page_with_spoilers) { FactoryBot.create(:page, source: "1. Question1\r\n    <details>\r\n      <summary>Answer</summary>\r\n      this is me, answer!\r\n    </details>") }
+ context 'markup' do
+  let(:page_with_russian_headers) { FactoryBot.create(:page, source: "# Привет, мир 2!\r\nHello, world!") }
+  let(:page_with_spoilers) { FactoryBot.create(:page, source: "1. Question1\r\n<details>\r\n<summary>Answer</summary>\r\nthis is me, answer!\r\n</details>") }
 
-    it 'render headers with anchors' do
-      expect(page_with_russian_headers.html).to eq("<h1 id=\"privet-mir-2\">Привет, мир 2!</h1>\n<p>Hello, world!</p>\n")
-    end
-
-    it 'render html with spoilers' do
-      expect(page_with_spoilers.html).to eq("<ol>\n<li>Question1\n <details>\n   <summary>Answer</summary>\n   this is me, answer!\n </details>\n</li>\n</ol>\n")
-    end
+  it 'render headers with anchors' do
+   expect(page_with_russian_headers.html).to eq("<h1 id=\"privet-mir-2\">Привет, мир 2!</h1>\n<p>Hello, world!</p>\n")
   end
+
+  it 'render html with spoilers' do
+   expect(page_with_spoilers.html).to eq("<ol>\n<li>Question1</li>\n</ol>\n<details>\n<summary>Answer</summary>\nthis is me, answer!\n</details>\n")
+  end
+ end
 end
