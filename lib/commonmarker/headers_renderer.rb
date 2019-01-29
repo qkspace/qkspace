aa078ddad32a2343f8efe22b778477164e78d089
gargantua <istroganovi@gmail.com>
2019-01-29 17:10:42 +0300

    fix problem in en locale

--- lib/commonmarker/headers_renderer.rb
+++ lib/commonmarker/headers_renderer.rb
@@ -3,7 +3,7 @@ require 'commonmarker'
 class HeadersRenderer < CommonMarker::HtmlRenderer
   def header(node)
     block do
-      out("<h", node.header_level, " id=\"", I18n.transliterate(node.first_child.string_content).parameterize.presence,
+      out("<h", node.header_level, " id=\"", I18n.transliterate(node.first_child.string_content, :locale => :ru).parameterize.presence,
           "\">", :children, "</h", node.header_level, ">")
     end
   end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -3,10 +3,22 @@ require 'rails_helper'
 RSpec.describe Page, type: :model do
   context 'markup' do
     let(:page_with_russian_headers) { FactoryBot.create(:page, source: "# Привет, мир 2!\r\nHello, world!") }
+    let(:page_with_english_headers) { FactoryBot.create(:page, source: "# Hello, world 2!\r\nHello, world!") }
+    let(:page_with_mix_headers) { FactoryBot.create(:page, source: "# Hello, world 2!\r\n## Привет, мир 2!\r\nHello, world!") }
     let(:page_with_spoilers) { FactoryBot.create(:page, source: "1. Question1\r\n<details>\r\n<summary>Answer</summary>\r\nthis is me, answer!\r\n</details>") }
 
     it 'render headers with anchors' do
+      expect(page_with_english_headers.html).to eq("<h1 id=\"hello-world-2\">Hello, world 2!</h1>\n<p>Hello, world!</p>\n")
+    end
+
+    it 'render headers in en locale' do
+      I18n.locale = :en
       expect(page_with_russian_headers.html).to eq("<h1 id=\"privet-mir-2\">Привет, мир 2!</h1>\n<p>Hello, world!</p>\n")
+      expect(I18n.locale).to eq :en # method doesn`t change current locale
+    end
+
+    it 'render headers with mix lang' do
+      expect(page_with_mix_headers.html).to eq("<h1 id=\"hello-world-2\">Hello, world 2!</h1>\n<h2 id=\"privet-mir-2\">Привет, мир 2!</h2>\n<p>Hello, world!</p>\n")
     end
 
     it 'render html with spoilers' do
