598d0b522c30b62fb2c433b95ede24835081b408
Eugene Zolotarev <eugzol@gmail.com>
2019-02-13 03:29:14 +0300

    HeaderRenderer now assigns different ids to headers with the same name

--- lib/commonmarker/headers_renderer.rb
+++ lib/commonmarker/headers_renderer.rb
@@ -1,10 +1,30 @@
 require 'commonmarker'
 
 class HeadersRenderer < CommonMarker::HtmlRenderer
+  def initialize(*args)
+    @headers_counter = Hash.new(0)
+    super(*args)
+  end
+
   def header(node)
     block do
-      out("<h", node.header_level, " id=\"", I18n.transliterate(node.first_child.string_content, :locale => :ru).parameterize.presence,
+      out("<h", node.header_level, " id=\"", header_id(node),
           "\">", :children, "</h", node.header_level, ">")
     end
+  end
+
+  private
+
+  def header_id(node)
+    id = I18n.
+      transliterate(node.first_child.string_content, locale: :ru).
+      parameterize.
+      presence
+
+    id << "-#{@headers_counter[id] + 1}" if @headers_counter[id] != 0
+
+    @headers_counter[id] += 1
+
+    id
   end
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Page, type: :model do
   context 'markup' do
-    let(:page_with_russian_headers) { FactoryBot.create(:page, source: "# Привет, мир 2!\r\nHello, world!") }
-    let(:page_with_english_headers) { FactoryBot.create(:page, source: "# Hello, world 2!\r\nHello, world!") }
-    let(:page_with_mix_headers) { FactoryBot.create(:page, source: "# Hello, world 2!\r\n## Привет, мир 2!\r\nHello, world!") }
-    let(:page_with_spoilers) { FactoryBot.create(:page, source: "1. Question1\r\n<details>\r\n<summary>Answer</summary>\r\nthis is me, answer!\r\n</details>") }
+    let(:page_with_russian_headers) { create(:page, source: "# Привет, мир 2!\r\nHello, world!") }
+    let(:page_with_english_headers) { create(:page, source: "# Hello, world 2!\r\nHello, world!") }
+    let(:page_with_mix_headers) { create(:page, source: "# Hello, world 2!\r\n## Привет, мир 2!\r\nHello, world!") }
+    let(:page_with_spoilers) { create(:page, source: "1. Question1\r\n<details>\r\n<summary>Answer</summary>\r\nthis is me, answer!\r\n</details>") }
+    let(:page_with_same_headers) { create(:page, source: "# Hello, world!\n## Hello, world!") }
 
     it 'render headers with anchors' do
       expect(page_with_english_headers.html).to eq("<h1 id=\"hello-world-2\">Hello, world 2!</h1>\n<p>Hello, world!</p>\n")
@@ -23,6 +24,10 @@ RSpec.describe Page, type: :model do
 
     it 'render html with spoilers' do
       expect(page_with_spoilers.html).to eq("<ol>\n<li>Question1</li>\n</ol>\n<details>\n<summary>Answer</summary>\nthis is me, answer!\n</details>\n")
+    end
+
+    it "assigns different IDs to headers with the same text" do
+      expect(page_with_same_headers.html).to eq "<h1 id=\"hello-world\">Hello, world!</h1>\n<h2 id=\"hello-world-2\">Hello, world!</h2>\n"
     end
   end
 end
