5a3ebdc4f2ab9ca62c554c9ffe665929501871f3
gargantua <istroganovi@gmail.com>
2019-02-14 11:22:28 +0300

    refactoring codestyle

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -10,21 +10,21 @@ RSpec.describe Page, type: :model do
     let(:page_with_tricky_headers) { create(:page, source: "# Hello, world!\n## Hello, world 2!\n## Hello, world!") }
 
     it 'renders headers with anchors' do
-      expect(page_with_english_headers.html).to eq("<h1 id=\"hello-world-2\">Hello, world 2!</h1>\n<p>Hello, world!</p>\n")
+      expect(page_with_english_headers.html).to eq "<h1 id=\"hello-world-2\">Hello, world 2!</h1>\n<p>Hello, world!</p>\n"
     end
 
     it 'renders headers in en locale' do
       I18n.locale = :en
-      expect(page_with_russian_headers.html).to eq("<h1 id=\"privet-mir-2\">Привет, мир 2!</h1>\n<p>Hello, world!</p>\n")
+      expect(page_with_russian_headers.html).to eq "<h1 id=\"privet-mir-2\">Привет, мир 2!</h1>\n<p>Hello, world!</p>\n"
       expect(I18n.locale).to eq :en # method doesn`t change current locale
     end
 
     it 'renders headers with mix lang' do
-      expect(page_with_mix_headers.html).to eq("<h1 id=\"hello-world-2\">Hello, world 2!</h1>\n<h2 id=\"privet-mir-2\">Привет, мир 2!</h2>\n<p>Hello, world!</p>\n")
+      expect(page_with_mix_headers.html).to eq "<h1 id=\"hello-world-2\">Hello, world 2!</h1>\n<h2 id=\"privet-mir-2\">Привет, мир 2!</h2>\n<p>Hello, world!</p>\n"
     end
 
     it 'renders html with spoilers' do
-      expect(page_with_spoilers.html).to eq("<ol>\n<li>Question1</li>\n</ol>\n<details>\n<summary>Answer</summary>\nthis is me, answer!\n</details>\n")
+      expect(page_with_spoilers.html).to eq "<ol>\n<li>Question1</li>\n</ol>\n<details>\n<summary>Answer</summary>\nthis is me, answer!\n</details>\n"
     end
 
     it 'assigns different IDs to headers with the same text' do
