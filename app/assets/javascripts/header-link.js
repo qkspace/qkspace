e7780e83480abbba1e035f2aa2f9f6e6031a066d
gargantua <istroganovi@gmail.com>
2019-02-19 20:03:39 +0300

    refactoring header-links

--- app/assets/javascripts/header-link.js
+++ app/assets/javascripts/header-link.js
@@ -1,12 +1,12 @@
 $(document).on('turbolinks:load', function () {
-    $("h1, h2, h3, h4, h5, h6").each(function(i, el) {
-        var $el, id;
-        $el = $(el);
-        id = $el.attr('id');
+  $("h1, h2, h3, h4, h5, h6").each(function(i, el) {
+    var $el, id;
+      $el = $(el);
+      id = $el.attr('id');
 
-        if (id !== undefined) {
-            $el.prepend($("<a href='#" + id + "' class='header-link'>")
-                .html('#'));
-        }
-    });
+      if (id !== undefined) {
+        $el.prepend($("<a href='#" + id + "' class='header-link'>")
+          .html('#'));
+      }
+  });
 });
