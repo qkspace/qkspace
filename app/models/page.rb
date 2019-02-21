fe317774c3254b55eb1d0aaf6bef080ea2513bad
gargantua <istroganovi@gmail.com>
2019-02-21 10:03:33 +0300

    fixed slug bug in en locale

--- app/models/page.rb
+++ app/models/page.rb
@@ -27,7 +27,7 @@ class Page < ApplicationRecord
   private
 
   def generate_slug
-    self.slug = I18n.transliterate(title.to_s).parameterize.presence
+    self.slug = I18n.transliterate(title.to_s, locale: :ru).parameterize.presence
   end
 
   def markup
