baa7e82444d84db7af6e05c2ec4a9f4e06c8c39f
Mikhail Berkovich <combos93@mail.ru>
2019-09-22 21:44:41 +0600

    tests for mistake_mailer/controller

--- spec/mailers/mistake_spec.rb
+++ spec/mailers/mistake_spec.rb
@@ -1,16 +1,29 @@
 require "rails_helper"
-
+    
 RSpec.describe MistakeMailer, :type => :mailer do
   before { ActionMailer::Base.deliveries = [] }
 
-  let(:text_msg) { 'Mistake is here!' }
-  let(:url) { 'qwerty.qkspace.com' }
-  let(:authors) { [['userone@mail.ru'], ['usertwo@mail.ru']] }
-  let(:title) { 'Reactive' }
-
   describe "send of finded mistake" do
-    let(:mail) { MistakeMailer.mistake(text_msg, url, authors[0], title).deliver_now }
-    let(:mail2) { MistakeMailer.mistake(text_msg, url, authors[1], title).deliver_now }
+    text_msg = 'Mistake is here!'
+    url = 'qwerty.qkspace.com'
+    authors = [['userone@mail.ru'], ['usertwo@mail.ru']]
+    title = 'Reactive'
+    locales = ["ru", "en"]
+    
+    mail = MistakeMailer.with(text_msg: text_msg,
+                              url: url,
+                              author: authors[0],
+                              title: title,
+                              locale: locales[0])
+                              .mistake
+                              .deliver
+    mail2 = MistakeMailer.with(text_msg: text_msg,
+                               url: url,
+                               author: authors[1],
+                               title: title,
+                               locale: locales[1])
+                               .mistake
+                               .deliver
 
     it "renders the sender email" do
       expect(mail.from).to eq(["no-reply@qkspace.com"])
@@ -18,7 +31,8 @@ RSpec.describe MistakeMailer, :type => :mailer do
 
     it "renders the reciever email" do
       expect(mail.to).to eq(authors[0])
-      expect(mail2.to).to eq(authors[1])
+      expect(mail2.to).to eq(authors[1])      
+
       expect(MistakeMailer.deliveries.size).to eq(2)
     end
 
@@ -29,8 +43,7 @@ RSpec.describe MistakeMailer, :type => :mailer do
     it 'renders the body' do
       expect(mail.to).to eq(authors[0])
 
-      message = ActionMailer::Base.deliveries.last
-      expect(message.html_part.body.to_s).to include('is here!')
+      expect(mail.html_part.body.raw_source).to include("Найдена ошибка")
     end
   end
 end
