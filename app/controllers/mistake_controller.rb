788a9c8fa7642608dd885e578cc0c372cd82fe2d
Mikhail Berkovich <combos93@mail.ru>
2019-09-22 21:29:40 +0600

    redo mistake_controller

--- app/controllers/mistake_controller.rb
+++ app/controllers/mistake_controller.rb
@@ -1,16 +1,54 @@
 class MistakeController < PublicController
+  before_action :locale_from_headers, :set_locale, only: [:create]
+
   def create
-    #set_project - this method from PublicController
-    @project_title = set_project.title
+    text_msg = mistake_params[:content]
+    url = mistake_params[:url]
 
-    owner = set_project.owner.email
-    collaborators = set_project.collaborators.map(&:email)
-    @authors = [owner] + collaborators
+    title = @project.title
 
-    @authors.each do |author|
-      MistakeMailer.mistake(params[:content], params[:url], author, @project_title).deliver
+    owner = @project.owner.email
+    collaborators = @project.collaborators.pluck(:email)
+
+    locales = ([@project.owner] + @project.collaborators).map(&:locale) 
+    emails = ([@project.owner] + @project.collaborators).map(&:email)
+    
+    emails.each_with_index do |author, index|
+      MistakeMailer.
+        with(
+          text_msg: text_msg,
+          url: url,
+          author: author,
+          title: title,
+          locale: locales[index]
+        ).
+        mistake.
+        deliver
     end
 
-    redirect_to root_path, notice: "Ваше сообщение с ошибкой успешно отправлено"
+    redirect_to root_path, notice: I18n.t('mistake_controller.create.success')
+  end
+
+  private
+
+  def mistake_params
+    params.require(:mistake).permit(:content, :url)
+  end
+
+  def locale_from_headers
+    request.env["HTTP_ACCEPT_LANGUAGE"].to_s.scan(/\A[a-z]{2}\z/i).first
+  end
+
+  def set_locale
+    locale =
+      [params[:locale], current_user&.locale, locale_from_headers, I18n.default_locale]
+      .compact
+      .map(&:to_sym)
+      .select { |l| I18n.available_locales.include?(l) }
+      .first
+
+    current_user&.update(locale: locale) unless current_user&.locale == locale
+
+    I18n.locale = locale
   end
 end
