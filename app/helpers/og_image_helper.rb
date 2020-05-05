04e5e1ac76a7e1d7b1815a6253225a831fc753d1
Sou1Jkeee <1nfern0@list.ru>
2020-05-06 02:16:32 +0300

    OgImageHelper fixes

--- app/helpers/og_image_helper.rb
+++ app/helpers/og_image_helper.rb
@@ -7,26 +7,17 @@ module OgImageHelper
     title.font_weight = Magick::NormalWeight
     title.fill = 'white'
     title.gravity = Magick::CenterGravity
-    title.annotate(img, 0, 0, 0, -150, @page.title)
+    title.annotate(img, 0, 0, 0, 0, @page.title)
 
     descritpion = Magick::Draw.new
-    descritpion.pointsize = 72
+    descritpion.pointsize = 64
     descritpion.font_weight = Magick::NormalWeight
     descritpion.fill = 'white'
-    descritpion.gravity = Magick::CenterGravity
-    descritpion.annotate(img, 0, 0, 0, 300, wrap_text((page_descritpion(@page))))
+    descritpion.gravity = Magick::SouthEastGravity
+    descritpion.annotate(img, 0, 0, 100, 100, "#{request.original_url}")
 
     dir = "public/images/#{@project.id}/#{@page.id}"
 
     File.exists?(dir) ? img.write("#{dir}/og-image.png") : (FileUtils.makedirs(dir) && img.write("#{dir}/og-image.png"))
-  end
-
-  def wrap_text(text, col = 50)
-    text.gsub(/(.{1,#{col}})( +|$\n?)|(.{1,#{col}})/, "\\1\\3\n")
-  end
-
-  def page_descritpion(page)
-    result = sanitize(page.html, tags: [], attributes: [])
-    truncate(result, length: 350, separator: ' ', omission: ' ...')
   end
 end
