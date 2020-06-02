a805359ac21111b0cad2150d811b1cf40171074c
Sou1Jkeee <1nfern0@list.ru>
2020-06-02 21:17:04 +0300

    fixed sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/0' }
+  config.redis = { url: 'redis://127.0.0.1:6379/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/0' }
+  config.redis = { url: 'redis://127.0.0.1:6379/0' }
 end
