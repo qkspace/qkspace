3edd9f3492c7e0e34571d0409a8b5fa0a2c00262
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-07-07 21:04:15 +0300

    Added possibility create pages in file seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 User.create(email: 'example@example.com')
 
-Project.create(id: '1', user_id: '1', title: 'my_project', slug: 'bbbb', google_analytics_tracker_id: 'UA-124200793-2',
-  google_remarketing_tracker_id: 'AW-74378077', yandex_metrika_tracker_id: '5555231',
+Project.create(id: '1', user_id: '1', title: 'test_project', slug: 'bbbb',
   private: '1', secret_enabled: '1')
+
+Page.create(title: 'about_qkspace_page', source: 'QKspace (произносится как «быстрое
+ пространство») является минималистической базой знаний для небольших команд.',
+ id: '2', project_id: '1')
+
+Page.create(title: 'description_qkspace_page', source: 'Он идеально подходит
+  для создания справочной базы курсов для образовательной организации, для
+  составления списка QnA в ИТ, для обмена знаниями в социальных группах, для
+  cоставления тематического списка личных цитат и заметок.',
+  id: '3', project_id: '1')
